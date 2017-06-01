35ea5237b59c5c1674327369ae4660c440258e46
LiquidWater <LiquidWater@192.168.2.4>
2017-06-01 15:09:05 -0400

    backing up work

--- BNetClassicChat_CmdLine/BNetClassicChat_CmdLine.cs
+++ BNetClassicChat_CmdLine/BNetClassicChat_CmdLine.cs
@@ -12,11 +12,23 @@ namespace BNetClassicChat_CmdLine
     {
         static void Main(string[] args)
         {
+            var lastRequestId = 0;
+            var apiKey = "test";
             //BNetClassicChatClient client = new BNetClassicChatClient("blargh");
-            using (var ws = new WebSocket("wss://connect-bot.classic.blizzard.com/v1/rpc/chat"))
+            using (var ws = new WebSocket("wss://connect-bot.classic.blizzard.com/v1/rpc/chat", "json"))
             {
-                ws.OnOpen += (sender, e) => { Console.WriteLine("Opened!"); };
+                ws.OnOpen += (sender, e) =>
+                {
+                    Console.WriteLine("Opened!");
+                    var auth = "{command: Botapiauth.AuthenticateRequest\nrequest_id: " + lastRequestId + "\npayload: {api_key" + apiKey + "\n}\n}";
+                    ws.Send(auth);
+                };
+                ws.OnMessage += (sender, e) =>
+                {
+                    Console.WriteLine(e.Data);
+                };
                 ws.OnError += (sender, e) => { throw e.Exception; };
+                ws.OnClose += (sender, e) => { Console.WriteLine("Closed!"); };
                 ws.Connect();
             }
             Console.ReadLine();
