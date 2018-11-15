e67ad229e88f27662d2f20b1fbdb43b9a57ce5fb
LiquidWater <bluemoon_redsun@hotmail.com>
2018-11-15 15:45:45 -0500

    updated copyright and license stuff

--- BNetClassicChat_ClientAPI/BNetClassicChat_ClientAPI.cs
+++ BNetClassicChat_ClientAPI/BNetClassicChat_ClientAPI.cs
@@ -1,4 +1,23 @@
-﻿using BNetClassicChat_ClientAPI.Resources;
+﻿/*
+    BNetClassicChat_ClientAPI.cs: Main entry point for the API wrapper
+
+    Copyright (C) 2018 LiquidWater
+    https://github.com/Liquidwater
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using BNetClassicChat_ClientAPI.Resources;
 using BNetClassicChat_ClientAPI.Resources.EArgs;
 using BNetClassicChat_ClientAPI.Resources.Models;
 using Newtonsoft.Json;
